103fa0e9b7df7052029a068e1e96b6de0b1d2127
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-07-27 18:22:24 +0300

    init Spring-Boot-App

---
+++ src/main/java/com/springbootdto/SpringBootDTOApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDTOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDTOApplication.class, args);
+	}
+
+}
